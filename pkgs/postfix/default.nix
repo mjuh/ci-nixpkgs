74ed644676ba6caa3a2118101b2ff67b20a83b8c
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-06-28 18:40:04 +0300

    withlib

--- pkgs/postfix/default.nix
+++ pkgs/postfix/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, wrapProgram, db, openssl, cyrus_sasl, icu, libnsl, pcre }:
+{ stdenv, fetchurl, wrapProgram, db, openssl, cyrus_sasl, icu, libnsl, pcre, lib }:
 
 stdenv.mkDerivation rec {
       name = "postfix-${version}";
