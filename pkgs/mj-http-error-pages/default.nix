d85b809265f17bfab6bb3f4417caca0eefc08609
sidorov <sidorov@majordomo.ru>
2019-07-02 18:07:33 +0300

    remove rev

--- pkgs/mj-http-error-pages/default.nix
+++ pkgs/mj-http-error-pages/default.nix
@@ -6,7 +6,6 @@ stdenv.mkDerivation rec {
   src = fetchGit {
     url = "git@gitlab.intr:shared/http_errors.git";
     ref = "master";
-    rev = "f83136c7e6027cb28804172ff3582f635a8d2af7";
   };
   outputs = [ "out" ];
   postInstall = ''
