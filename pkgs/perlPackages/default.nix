ae477e4a2b96e7f27411f9f8898f52053030db82
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-07-27 02:03:42 +0300

    2

--- pkgs/perlPackages/default.nix
+++ pkgs/perlPackages/default.nix
@@ -1,7 +1,9 @@
 { perl,
   fetchurl,
   pkgconfig,
-  buildPerlPackage }:
+  buildPerlPackage,
+  config, pkgs, fetchFromGitHub, stdenv, gnused, overrides,
+  buildPerl }:
 
 {
   TextTruncate = buildPerlPackage rec {
@@ -10,7 +12,7 @@
       url = "mirror://cpan/authors/id/I/IL/ILV/${name}.tar.gz";
       sha256 = "1933361ec297253d1dd518068b863dcda131aba1da5ac887040c3d85a2d2a5d2";
     };
-    buildInputs = [ buildPerlPackage.ModuleBuild ];
+    buildInputs = [ ModuleBuild ];
   };
 
 }
