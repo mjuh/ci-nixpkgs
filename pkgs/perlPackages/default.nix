38dc83b152effcfb467bf93db04013e7cdfdb199
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-07-27 01:58:36 +0300

    1

--- pkgs/perlPackages/default.nix
+++ pkgs/perlPackages/default.nix
@@ -10,7 +10,7 @@
       url = "mirror://cpan/authors/id/I/IL/ILV/${name}.tar.gz";
       sha256 = "1933361ec297253d1dd518068b863dcda131aba1da5ac887040c3d85a2d2a5d2";
     };
-    buildInputs = [ ModuleBuild ];
+    buildInputs = [ buildPerlPackage.ModuleBuild ];
   };
 
 }
