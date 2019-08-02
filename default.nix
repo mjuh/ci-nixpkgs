4cc70da3ee601b192ba8b2b793c8db34854b52bb
Oleg Pykhalov <go.wigust@gmail.com>
2019-08-02 18:51:45 +0300

    rename pureftpd to pure-ftpd as in nixpkgs official repo

--- default.nix
+++ default.nix
@@ -19,7 +19,7 @@ let
     luajitPackages = super.luajitPackages // (callPackage ./pkgs/luajit-packages { lua = openrestyLuajit2; });
     perlPackages = super.perlPackages // (callPackage ./pkgs/perlPackages {});
     ioncube = callPackage ./pkgs/ioncube {};
-    pureftpd = callPackage ./pkgs/pureftpd {};
+    pure-ftpd = callPackage ./pkgs/pure-ftpd {};
   };
 
 in
