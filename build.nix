e7f65f203c06beab5d634f809bdd0e6346091199
Oleg Pykhalov <go.wigust@gmail.com>
2019-08-09 15:27:57 +0300

    Build Perl packages

--- build.nix
+++ build.nix
@@ -9,12 +9,10 @@ with import <nixpkgs> {};
 
 let
   # TODO: convert to callPackages and non-overlay style? more reliable and usable by others, but can cause more pkg dupe?
-  nixpkgsUnstable = lib.filterAttrs (p: v: p != "mjperl5Packages")
-    (lib.filterAttrs (p: v: p != "perlPackages")
-      (lib.filterAttrs (p: v: p != "luajitPackages")
-        (import
-          (import ./nixpkgs/nixpkgs-unstable)
-          { overlays = [ (import ./default.nix) ]; }).majordomoPkgs));
+  nixpkgsUnstable = lib.filterAttrs (p: v: p != "luajitPackages")
+    (import
+      (import ./nixpkgs/nixpkgs-unstable)
+      { overlays = [ (import ./default.nix) ]; }).majordomoPkgs;
 in
 {
   all = [ nixpkgsUnstable ];
