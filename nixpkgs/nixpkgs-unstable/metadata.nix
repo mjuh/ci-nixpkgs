b492493fc52ecfc5ef9aead16f18ec1f688a352b
Oleg Pykhalov <go.wigust@gmail.com>
2019-08-23 18:28:21 +0300

    nixpkgs-unstable: metadata.nix: Update to 88d9f776

--- nixpkgs/nixpkgs-unstable/metadata.nix
+++ nixpkgs/nixpkgs-unstable/metadata.nix
@@ -1,5 +1,5 @@
 {
-  rev = "88d9f776091896cfe57dc6fbdf246e7d27d5f105";
-  sha256 = "0z8a0g69fmbbzi77jhvhwafv73dn5fg3gsr0q828lss6j5qpx995";
-  revdate = "2019-07-25T10:04:17Z";
+  rev = "8120fb6ec4c52cf3acda82d968ad9fd0bd5f0ef7";
+  sha256 = "0wawyvb211cj21564sa44h5xvkz4hk2kn1j412njrvch3nfdrwlb";
+  revdate = "2019-08-23T10:04:17Z";
 }
