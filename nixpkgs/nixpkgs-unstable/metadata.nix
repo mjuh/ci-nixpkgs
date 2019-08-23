0d2337e849daef4f36642f108959b603883e5649
Oleg Pykhalov <go.wigust@gmail.com>
2019-08-23 18:29:56 +0300

    Fix nixpkgs-unstable sha256

--- nixpkgs/nixpkgs-unstable/metadata.nix
+++ nixpkgs/nixpkgs-unstable/metadata.nix
@@ -1,5 +1,5 @@
 {
   rev = "8120fb6ec4c52cf3acda82d968ad9fd0bd5f0ef7";
-  sha256 = "0wawyvb211cj21564sa44h5xvkz4hk2kn1j412njrvch3nfdrwlb";
+  sha256 = "18jnfnw4q465bswv03xfsm5h53mvci238jpyhzdzwhzfl8xxym2s";
   revdate = "2019-08-23T10:04:17Z";
 }
