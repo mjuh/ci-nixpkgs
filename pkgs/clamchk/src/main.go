e5ab43c4e8519d2f7d0cb9fa7b738dd320c80e0a
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-09-05 13:36:44 +0300

    Пропускать файлы с размером более 100 М

--- pkgs/clamchk/src/main.go
+++ pkgs/clamchk/src/main.go
@@ -1,46 +1,50 @@
 package main
 
 import (
-        "bytes"
-        "fmt"
-        "github.com/dutchcoders/go-clamd"
-        "io/ioutil"
-        "os"
-        "path/filepath"
+	"bytes"
+	"fmt"
+	"github.com/dutchcoders/go-clamd"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-        err := filepath.Walk(".",
-                func(path string, info os.FileInfo, err error) error {
-                        if err != nil {
-                                return err
-                        }
-                        //    fmt.Println(path, info.Size())
-                        if !info.IsDir() {
-                                //      continue
-                                myscan(path)
-                        }
-                        return nil
-                })
-        if err != nil {
-                fmt.Printf("%v\n", err)
-        }
+	err := filepath.Walk(".",
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			//    fmt.Println(path, info.Size())
+			if !info.IsDir() {
+				//	continue
+				//				fmt.Println(path, info.Size())
+				if info.Size() < 104857600 {
+					myscan(path)
+				} else {
+					fmt.Println(path, " Skiped(file size", info.Size(), " is too large)\n")
+				}
+			}
+			return nil
+		})
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
 }
 
 func myscan(scanfile string) {
-        c := clamd.NewClamd("tcp://clamav.intr:3310")
-        _ = c
+	c := clamd.NewClamd("tcp://clamav.intr:3310")
+	_ = c
 
-        fmt.Printf(scanfile + "\t")
-        filebyte, err := ioutil.ReadFile(scanfile)
-        if err != nil {
-                fmt.Printf("ReadFile: %v\n", err)
-        }
-        reader := bytes.NewReader(filebyte)
-        response, err := c.ScanStream(reader, make(chan bool))
+	fmt.Printf(scanfile + "\t")
+	filebyte, err := ioutil.ReadFile(scanfile)
+	if err != nil {
+		fmt.Printf("ReadFile: %v\n", err)
+	}
+	reader := bytes.NewReader(filebyte)
+	response, err := c.ScanStream(reader, make(chan bool))
 
-        for s := range response {
-                fmt.Printf("%v %v\n", s, err)
-        }
+	for s := range response {
+		fmt.Printf("%v %v\n", s, err)
+	}
 }
-
