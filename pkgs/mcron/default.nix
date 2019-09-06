61ec1925dac063949dcab0a1c39e7e8a8da3bbb1
Oleg Pykhalov <go.wigust@gmail.com>
2019-09-06 13:27:38 +0300

    pkgs: mcron: Don't send emails
    * pkgs/mcron/0003-mcron-vixie-specification-Don-t-send-emails.patch:
    New file.
    * pkgs/mcron/default.nix: Use this.

--- pkgs/mcron/default.nix
+++ pkgs/mcron/default.nix
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
 
   patches = [ ./0001-base-Display-job-to-STDOUT.patch
               ./0002-scripts-cron-Don-t-daemonize.patch
-            ];
+              ./0003-mcron-vixie-specification-Don-t-send-emails.patch ];
 
   buildInputs = [ guile which ed libtool pkg-config ];
 
