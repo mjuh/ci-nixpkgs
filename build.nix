7aaa514a6bfe28870a411792dd8d058f5baea70c
Oleg Pykhalov <go.wigust@gmail.com>
2019-09-24 17:12:36 +0300

    Use only <nixpkgs>.

--- build.nix
+++ build.nix
@@ -1,16 +1,11 @@
 # This file contains machinery to build all packages in the overlay.
 # To do that, run:
 #
-# nix-build build.nix --cores 4 -A nixpkgsUnstable --show-trace
+# nix-build build.nix --cores 4 -A php73 --show-trace
 #
 # The results are directory hierarchies.
 
-let
-  nixpkgsUnstable =
-    (import <nixpkgs>
-      { overlays = [ (import ./default.nix) ]; }).majordomoPkgs;
-in
-{
-  all = [ nixpkgsUnstable ];
-  inherit nixpkgsUnstable;
-}
+with <nixpkgs>;
+with lib;
+
+import <nixpkgs> { overlays = [ (import ./default.nix) ]; }
