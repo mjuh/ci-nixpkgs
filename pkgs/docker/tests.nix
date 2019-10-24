92c1ef6bb925601efa05cae07dca0b7ac20e6a40
Oleg Pykhalov <go.wigust@gmail.com>
2019-10-24 16:20:08 +0300

    pkgs: docker: tests: Generate HTML output of wrk.

--- pkgs/docker/tests.nix
+++ pkgs/docker/tests.nix
@@ -221,8 +221,19 @@ let
       #!${bash}/bin/bash
       # Run wrk test.
       exec &> /tmp/xchg/coverage-data/wrk.html
-      set -e -x
+      cat <<EOF
+      <html>
+        <head>
+      <title>wrk</title>
+      </head>
+      <body>
+      <h1>wrk</h1>
+      EOF
       ${wrk2}/bin/wrk2 -t2 -c100 -d30s -R2000 http://${phpVersion}.ru/
+      cat <<EOF
+      </body>
+      </html>
+      EOF
     '';
 
     phpVersion = php2version php;
