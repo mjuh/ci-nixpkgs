662a7f46ffb292ea7f99d6c5d2ed84df543a1ee3
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-12-21 19:47:31 +0300

    mariadb-connector fix

--- pkgs/mariadb-connector-c/default.nix
+++ pkgs/mariadb-connector-c/default.nix
@@ -1,8 +1,14 @@
 { stdenv, mariadb, symlinkJoin, openssl }:
 
 let
-  mariadb-connector-c = (mariadb.override{ inherit openssl; }).connector-c.overrideAttrs (_: rec {
+  mariadb-connector-c = (mariadb.override{ inherit openssl; }).connector-c.overrideAttrs (oldAttrs: {
     patches = [ ./php_mysqli.so_required_symbols_export.patch ];
+    postInstall = oldAttrs.postInstall or "" + ''
+              ln -sf $out/lib/mariadb/libmysqlclient.so  $out/lib/mariadb/libmysqlclient.so.18
+              ln -sf $out/lib/mariadb/libmysqlclient.so  $out/lib/mariadb/libmysqlclient.so.18.0.0
+              ln -sf $out/lib/mariadb/libmysqlclient.so  $out/lib/mariadb/libmysqlclient_r.so.18
+              ln -sf $out/lib/mariadb/libmysqlclient.so  $out/lib/mariadb/libmysqlclient_r.so.18.0.0
+    '';
   });
 in
   
