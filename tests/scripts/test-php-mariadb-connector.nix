be667fa4dbf63de60241b03c817d95e25582637c
Oleg Pykhalov <go.wigust@gmail.com>
2019-12-23 14:49:57 +0300

    tests: Move tests to apache2 projects.

---
+++ tests/scripts/test-php-mariadb-connector.nix
@@ -0,0 +1,8 @@
+{ pkgs }:
+
+with pkgs;
+
+writeScript "test-php-mariadb-connector.sh" ''
+  #!${bash}/bin/bash
+  ${php}/bin/php -r '$mysqlnd = function_exists("mysqli_fetch_all"); if ($mysqlnd) {exit (0);} else {exit (1);}'
+''
