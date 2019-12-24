62b66e4d4a0d522fe2eef9c5c92c7f7a595d8a46
Oleg Pykhalov <go.wigust@gmail.com>
2019-12-24 17:17:03 +0300

    tests: Add images.nix.

---
+++ tests/images.nix
@@ -0,0 +1,10 @@
+[
+  {
+    imageName = "gcr.io/gcp-runtimes/container-structure-test";
+    imageDigest =
+      "sha256:cc5ff23f5c8d18e532664c6de2eab47e0d621b55b30ad47a1d4ee134220b3657";
+    sha256 = "0m24qrlzarwwd5dpp6w6l38qr8wl4qcb0p67bmd0wigbzwxhhqm3";
+    finalImageName = "gcr.io/gcp-runtimes/container-structure-test";
+    finalImageTag = "latest";
+  }
+]
