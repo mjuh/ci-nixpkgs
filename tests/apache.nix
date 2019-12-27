87c2f12e9527618888b5b60c32c6c3e97713fda8
Oleg Pykhalov <go.wigust@gmail.com>
2019-12-27 13:23:45 +0300

    tests: apache: Delete defaultTestSuite.

--- tests/apache.nix
+++ tests/apache.nix
@@ -1,8 +1,8 @@
 { pkgs, firefox, debug ? false, bash, image, jq, lib, php, phpinfoCompare
 , rootfs, stdenv, wordpress, wrk2, writeScript, python3, deepdiff
-, defaultTestSuite ? true, containerStructureTestConfig, phpinfo, testDiffPy
-, wordpressScript, wrkScript, dockerNodeTest, containerStructureTest
-, testImages, testSuite ? [ ], runCurl }:
+, containerStructureTestConfig, phpinfo, testDiffPy, wordpressScript, wrkScript
+, dockerNodeTest, containerStructureTest, testImages, testSuite ? [ ], runCurl
+}:
 
 # Run virtual machine, then container with Apache and PHP, and test it.
 
@@ -57,28 +57,30 @@ in import maketest ({ pkgs, lib, ... }: {
           isNormalUser = true;
           password = "admin";
           extraGroups = [ "wheel" ];
-          openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC35V1L3xgGA7DIdUCRzLpOjeaUi+qt2Gb5DV67ifmyw5i7UH9onVen6FQKMkuQBn0hVu9h9XVV30lGtCXj7Des/FXpV9OW3MRbWKxmekDshZHu+QkhZer9i+Nd413Q+UDzJGANUwJ71mr3H3rqTDZnWN/LQGkGv8xR/mK4vWfAWgidi1sdABgoEh0gN80Oa2VMhsX0Nx2VmCv5k7mftajADKnTc6paZGzCaShNgTlExZEHRfUUqXb1Yk3gifIZNxhdbcplmRLeMccbYnv0i7cg8TekH+3VmeS+JWNYVHrxHuic/L9otSL7HXnmnlAawmMQLZPUTCIMQP4kWju7I0BGCC2tUpm8OZvxtXtbHjLk5oX+Oe66IV142ORwvH/mWnOWs9JeafcGpP1VerpBTmPeQi3flbKOGCAGCeQtnLO8XPIx6FzNAfzrJJX05tvzo5Hau9ukDwLEzk+0+fb1biJOqbYFdwoj/mFBIwKLzAoOGw2v7UZmLAzPUFaKiZki4NgRpq5Myrc1xcn39h5jjMZU1K29lVxbMjYE3ikHrUOEYCsPQwL5KEKVIQRqk6UZY4FxApeSgoiCftbWGXpjheCHOUyd9+MtlL/Q1aWbxkMT5eNmFvMVm7hfLZqXje0lAnJJOX+KgyoHnNqmAmZsauUreW/mfhTFHFTCG8wpW50s3w==" ];
+          openssh.authorizedKeys.keys = [
+            "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC35V1L3xgGA7DIdUCRzLpOjeaUi+qt2Gb5DV67ifmyw5i7UH9onVen6FQKMkuQBn0hVu9h9XVV30lGtCXj7Des/FXpV9OW3MRbWKxmekDshZHu+QkhZer9i+Nd413Q+UDzJGANUwJ71mr3H3rqTDZnWN/LQGkGv8xR/mK4vWfAWgidi1sdABgoEh0gN80Oa2VMhsX0Nx2VmCv5k7mftajADKnTc6paZGzCaShNgTlExZEHRfUUqXb1Yk3gifIZNxhdbcplmRLeMccbYnv0i7cg8TekH+3VmeS+JWNYVHrxHuic/L9otSL7HXnmnlAawmMQLZPUTCIMQP4kWju7I0BGCC2tUpm8OZvxtXtbHjLk5oX+Oe66IV142ORwvH/mWnOWs9JeafcGpP1VerpBTmPeQi3flbKOGCAGCeQtnLO8XPIx6FzNAfzrJJX05tvzo5Hau9ukDwLEzk+0+fb1biJOqbYFdwoj/mFBIwKLzAoOGw2v7UZmLAzPUFaKiZki4NgRpq5Myrc1xcn39h5jjMZU1K29lVxbMjYE3ikHrUOEYCsPQwL5KEKVIQRqk6UZY4FxApeSgoiCftbWGXpjheCHOUyd9+MtlL/Q1aWbxkMT5eNmFvMVm7hfLZqXje0lAnJJOX+KgyoHnNqmAmZsauUreW/mfhTFHFTCG8wpW50s3w=="
+          ];
         };
       };
 
       security.sudo.enable = true;
       security.sudo.extraConfig = ''
-         admin  ALL=NOPASSWD: ALL
+        admin  ALL=NOPASSWD: ALL
       '';
 
       services.openssh.enable = if debug then true else false;
       services.openssh.permitRootLogin = if debug then "yes" else "no";
       environment.systemPackages = with pkgs; [ rsync mc tree jq ];
-    
+
       environment.variables.SECURITY_LEVEL = "default";
       environment.variables.SITES_CONF_PATH =
         "/etc/apache2-${phpVersion}-default/sites-enabled";
       environment.variables.SOCKET_HTTP_PORT = "80";
-      environment.interactiveShellInit = ''    
-         alias ll='ls -alF'
-         alias s='sudo -i'
-         alias show-tests='ls /nix/store/*{test,run,wordpress}*{sh,py}'
-         alias list-tests='ls /nix/store/*{test,run,wordpress}*{sh,py}'
+      environment.interactiveShellInit = ''
+        alias ll='ls -alF'
+        alias s='sudo -i'
+        alias show-tests='ls /nix/store/*{test,run,wordpress}*{sh,py}'
+        alias list-tests='ls /nix/store/*{test,run,wordpress}*{sh,py}'
       '';
       boot.initrd.postMountCommands =
         import ./boot-initrd-postMountCommands.nix { inherit phpVersion; };
@@ -96,14 +98,14 @@ in import maketest ({ pkgs, lib, ... }: {
       '';
       services.mysql.ensureDatabases = [ "wordpress" "oldpasswords" ];
       services.mysql.ensureUsers = [
-      {
-        name = "wordpress_user";
-        ensurePermissions = { "wordpress.*" = "ALL PRIVILEGES"; };
-      }
-      {
-        name = "old";
-        ensurePermissions = { "oldpasswords.*" = "ALL PRIVILEGES"; };
-      }
+        {
+          name = "wordpress_user";
+          ensurePermissions = { "wordpress.*" = "ALL PRIVILEGES"; };
+        }
+        {
+          name = "old";
+          ensurePermissions = { "oldpasswords.*" = "ALL PRIVILEGES"; };
+        }
       ];
       services.mysql.package = pkgs.mariadb;
     };
@@ -115,109 +117,14 @@ in import maketest ({ pkgs, lib, ... }: {
 
     print "Start services.\n";
     $dockerNode->waitForUnit("mysql");
-    $dockerNode->execute("${runDockerImage { inherit pkgs; inherit image; }}");
+    $dockerNode->execute("${
+      runDockerImage {
+        inherit pkgs;
+        inherit image;
+      }
+    }");
     $dockerNode->sleep(10);
   '']
-
-    ++ optionals defaultTestSuite [
-      (dockerNodeTest {
-        description = "Copy phpinfo.";
-        action = "execute";
-        command = "cp -v ${phpinfo} /home/u12/${domain}/www/phpinfo.php";
-      })
-      (dockerNodeTest {
-        description = "Fetch phpinfo.";
-        action = "succeed";
-        command = runCurl "http://${domain}/phpinfo.php"
-          "/tmp/xchg/coverage-data/phpinfo.html";
-      })
-      (dockerNodeTest {
-        description = "Fetch server-status.";
-        action = "succeed";
-        command = runCurl "http://127.0.0.1/server-status"
-          "/tmp/xchg/coverage-data/server-status.html";
-      })
-      (dockerNodeTest {
-        description = "Copy phpinfo-json.php.";
-        action = "succeed";
-        command = "cp -v ${
-            ./phpinfo-json.php
-          } /home/u12/${domain}/www/phpinfo-json.php";
-      })
-      (dockerNodeTest {
-        description = "Fetch phpinfo-json.php.";
-        action = "succeed";
-        command = runCurl "http://${domain}/phpinfo-json.php"
-          "/tmp/xchg/coverage-data/phpinfo.json";
-      })
-      (dockerNodeTest {
-        description = "Run deepdiff against PHP on Upstart.";
-        action = "succeed";
-        command = testDiffPy {
-          inherit pkgs;
-          sampleJson = (./. + "/${phpVersion}.json");
-          output = "/tmp/xchg/coverage-data/deepdiff.html";
-        };
-      })
-      (dockerNodeTest {
-        description = "Run deepdiff against PHP on Upstart with excludes.";
-        action = "succeed";
-        command = testDiffPy {
-          inherit pkgs;
-          sampleJson = (./. + "/${phpVersion}.json");
-          output = "/tmp/xchg/coverage-data/deepdiff-with-excludes.html";
-          excludes = import ./diff-to-skip.nix;
-        };
-      })
-      (dockerNodeTest {
-        description = "Run deepdiff against PHP on Nix.";
-        action = "succeed";
-        command = testDiffPy {
-          inherit pkgs;
-          sampleJson = (./. + "/web34/${phpVersion}.json");
-          output = "/tmp/xchg/coverage-data/deepdiff-web34.html";
-        };
-      })
-      (dockerNodeTest {
-        description = "Run deepdiff against PHP on Nix with excludes.";
-        action = "succeed";
-        command = testDiffPy {
-          inherit pkgs;
-          sampleJson = (./. + "/web34/${phpVersion}.json");
-          output = "/tmp/xchg/coverage-data/deepdiff-web34-with-excludes.html";
-          excludes = import ./diff-to-skip.nix;
-        };
-      })
-      (dockerNodeTest {
-        description = "Copy bitrix_server_test.php.";
-        action = "succeed";
-        command = "cp -v ${
-            ./bitrix_server_test.php
-          } /home/u12/${domain}/www/bitrix_server_test.php";
-      })
-      (dockerNodeTest {
-        description = "Run Bitrix test.";
-        action = "succeed";
-        command = runCurl "http://${domain}/bitrix_server_test.php"
-          "/tmp/xchg/coverage-data/bitrix_server_test.html";
-      })
-      (dockerNodeTest {
-        description = "Run container structure test.";
-        action = "succeed";
-        command = containerStructureTest {
-          inherit pkgs;
-          config = containerStructureTestConfig;
-          image = image.imageName;
-        };
-      })
-      (dockerNodeTest {
-        description = "Run mariadb connector test.";
-        action = "succeed";
-        command = "${testPhpMariadbConnector}";
-        # TODO: Run test in container, e.g: docker exec apache2-php73-default /opt/php73/bin/php -r '$mysqlnd = function_exists("mysqli_fetch_all"); if ($mysqlnd) {echo "mysqlnd enabled";} else {echo "No";}'
-
-      })
-    ]
 
     ++ testSuite
 
