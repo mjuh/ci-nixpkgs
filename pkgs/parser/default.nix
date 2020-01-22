c77119ef47781a2350dc53b07edaa7fbc05cdd35
Oleg Pykhalov <go.wigust@gmail.com>
2020-01-22 15:22:46 +0300

    pkgs: parser: Override fetchgit.
    * pkgs/parser/default.nix: Override fetchgit with local CA.

--- pkgs/parser/default.nix
+++ pkgs/parser/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   name = "parser3";
-  src = fetchgit {
+  src = fetchgit.override { inherit cacert; } {
     url = "https://gitlab.intr/webservices/parser-lebedeva.git";
     rev = "9d557f9ee4d018881e481aeb1dab52ea1a9882aa";
     sha256 = "1bv2lwyrz7sjcznpzhq6gb8bgcpmq0335pvjkqlw08ha7f6bbz4m";
