ad07c045dc666407b1df3d8a4123f286d5765723
Oleg Pykhalov <go.wigust@gmail.com>
2020-01-22 15:12:21 +0300

    pkgs: mj-http-error-pages: Override fetchgit.
    * pkgs/mj-http-error-pages/default.nix: Override fetchgit with local
    CA.

--- pkgs/mj-http-error-pages/default.nix
+++ pkgs/mj-http-error-pages/default.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation rec {
   name = "mjerrors";
   buildInputs = [ gettext ];
-  src = fetchgit {
+  src = fetchgit.override { inherit cacert; } {
     url = "https://gitlab.intr/shared/http_errors.git";
     rev = "25b81648ddbb77007540401324e7cc7c3a3d1ce1";
     sha256 = "0nai9z3nzhdllwqi3b2nb15mjyrbfc0jgbpyj86zgrpcl0wfgngx";
