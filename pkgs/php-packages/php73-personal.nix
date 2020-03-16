1d59232c5edb5f2cf112ab7ab3ad2e2d70547d15
Oleg Pykhalov <go.wigust@gmail.com>
2020-03-16 17:53:41 +0300

    pkgs: php73PersonalPackages: Add composer.

--- pkgs/php-packages/php73-personal.nix
+++ pkgs/php-packages/php73-personal.nix
@@ -1,7 +1,30 @@
-{ buildPhp73PersonalPackage, lib, pkgconfig, fontconfig, imagemagick
-, libmemcached, memcached, pcre2, rrdtool, zlib }:
+{ stdenv, buildPhp73PersonalPackage, lib, pkgconfig, fontconfig,
+  imagemagick , libmemcached, memcached, pcre2, rrdtool, zlib,
+  php73PersonalFpm, pkgs }:
 
 {
+  composer = stdenv.mkDerivation rec {
+    version = "1.9.0";
+    pname = "composer";
+
+    src = pkgs.fetchurl {
+      url = "https://getcomposer.org/download/${version}/composer.phar";
+      sha256 = "0x88bin1c749ajymz2cqjx8660a3wxvndpv4xr6w3pib16fzdpy9";
+    };
+
+    dontUnpack = true;
+
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      install -D $src $out/libexec/composer/composer.phar
+      makeWrapper ${php73PersonalFpm}/bin/php $out/bin/composer \
+        --add-flags "$out/libexec/composer/composer.phar" \
+        --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.unzip ]}
+    '';
+  };
+
   redis = buildPhp73PersonalPackage {
     name = "redis";
     version = "4.2.0";
