cd7c6ae8f5a9f3e428d348868ffc4d0e70335ac2
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2020-04-24 22:48:03 +0300

    deps for clamchk

--- pkgs/clamchk/deps.nix
+++ pkgs/clamchk/deps.nix
@@ -8,5 +8,13 @@
       sha256 = "149srsyj7z3lv6f31brjaz8g2sdgd8i12kf0iyy11p30xgcs9h60";
     };
   }
-
+  {
+    goPackagePath  = "gitlab.intr/go-hms/libs.git";
+    fetch = {
+       type = "git";
+       url = "git@gitlab.intr:go-hms/libs.git";
+       rev = "master";
+       sha256 = "1z8185gyfchbs1pvnri9sx4y0cypc37m5b3p6v6rfprvzfvls14y";
+    };
+  }
 ]
