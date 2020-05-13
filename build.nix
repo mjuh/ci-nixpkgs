f84f6404d842367e24371c134b119b7aba6e4c59
Oleg Pykhalov <go.wigust@gmail.com>
2020-05-13 17:03:37 +0300

    build.nix: Pin nixpkgs.

--- build.nix
+++ build.nix
@@ -6,11 +6,15 @@
 # Build specific package:
 # nix-build --show-trace --arg set true build.nix -A overlay.mjperl5Packages
 
-{ set ? false }:
+{ nixpkgs ? (import <nixpkgs> { }).fetchgit {
+  url = "https://github.com/NixOS/nixpkgs.git";
+  rev = "ce9f1aaa39ee2a5b76a9c9580c859a74de65ead5";
+  sha256 = "1s2b9rvpyamiagvpl5cggdb2nmx4f7lpylipd397wz8f0wngygpi";
+}, set ? false }:
 
 let
   overlay = import ./default.nix;
-  overlayed = import <nixpkgs> { overlays = [ overlay ]; };
+  overlayed = import nixpkgs { overlays = [ overlay ]; };
   justOverlayed = (overlay { } overlayed);
   lib = overlayed.lib;
   inherit (lib) collect filterAttrs isDerivation filter attrValues;
