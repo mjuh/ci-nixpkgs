1d9a8b2fbbb35e8eba6a6573241c9e122cd56a2a
Oleg Pykhalov <go.wigust@gmail.com>
2020-09-21 01:37:46 +0300

    pkgs: bashhub-client: Don't bind <C-b>.

--- pkgs/bashhub-client/default.nix
+++ pkgs/bashhub-client/default.nix
@@ -108,9 +108,12 @@ in with localPython.pkgs; buildPythonApplication rec {
   ];
   buildInputs = [ pytest ];
   # XXX: Don't hard-code Python version in BH_DEPS_DIRECTORY.
-  postUnpack = ''
-    substituteInPlace source/bashhub/shell/bashhub.sh \
+  prePatch = ''
+    substituteInPlace bashhub/shell/bashhub.sh \
       --replace '$HOME/.bashhub/bin' $out/bin \
-      --replace 'BH_DEPS_DIRECTORY=''${BH_DEPS_DIRECTORY:=$BH_HOME_DIRECTORY/deps}' 'BH_DEPS_DIRECTORY=$out/lib/python3.7/site-packages/bashhub/shell/deps'
+      --replace 'BH_DEPS_DIRECTORY=''${BH_DEPS_DIRECTORY:=$BH_HOME_DIRECTORY/deps}' BH_DEPS_DIRECTORY=$out/lib/python3.7/site-packages/bashhub/shell/deps
   '';
+  patches = [
+    ./bashhub-client-remove-keybind.patch
+  ];
 }
