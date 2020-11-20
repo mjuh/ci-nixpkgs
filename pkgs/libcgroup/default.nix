b91656a85ce5ee9da1ef9abdf86b9dc6736c9e43
Oleg Pykhalov <go.wigust@gmail.com>
2020-11-20 18:00:32 +0300

    pkgs: libcgroup: Fix version, use fetchgit, simplify and rename patch.

--- pkgs/libcgroup/default.nix
+++ pkgs/libcgroup/default.nix
@@ -1,22 +1,17 @@
-{ stdenv, fetchurl, fetchpatch, pam, yacc, flex, automake113x, autoconf
+{ stdenv, fetchgit, fetchpatch, pam, yacc, flex, automake113x, autoconf
 , autoconf-archive, automake, libtool }:
 
 stdenv.mkDerivation rec {
   pname = "libcgroup";
-  version = "master";
-
-  buildInputs =
-    [ pam yacc flex automake113x autoconf autoconf-archive automake libtool ];
-
-  src = fetchGit {
+  version = "0.42.2";
+  buildInputs = [ pam yacc flex autoconf autoconf-archive automake libtool ];
+  src = fetchgit {
     url = "https://github.com/libcgroup/libcgroup";
-    ref = "master";
+    rev = "v" + version;
+    sha256 = "16nxvv41clrms75bvhmdn304lj3fhrnivg0hdxj8rjm0a6r99kg1";
   };
-
-  patches = [ ./fix-loop.patch ];
-
+  patches = [ ./libcgroup-disable-tests.patch ];
   preConfigure = "autoreconf -fiv";
-
   meta = {
     description = "Library and tools to manage Linux cgroups";
     homepage = "http://libcg.sourceforge.net/";
