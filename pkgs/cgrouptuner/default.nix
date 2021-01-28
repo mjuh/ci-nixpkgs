2535fedddf353b08ff601e61fa2804cbdec3e8ba
Oleg Pykhalov <go.wigust@gmail.com>
2021-01-28 16:20:16 +0300

    pkgs: cgrouptuner: Prettify.

--- pkgs/cgrouptuner/default.nix
+++ pkgs/cgrouptuner/default.nix
@@ -3,22 +3,11 @@
 stdenv.mkDerivation {
   name = "cgrouptuner";
   src = ./src;
-
-  # XXX:
-  # buildPhase = ''
-  #   patchShebangs $out/bin/*
-  # '';
-
-  nativeBuildInputs = [
-    makeWrapper
-  ];
-
-  # XXX: Maybe in postInstall
+  nativeBuildInputs = [ makeWrapper ];
   installPhase = ''
     mkdir -p $out/bin
     install -m755 cgrouptuner.pl $out/bin/cgrouptuner
     wrapProgram $out/bin/cgrouptuner --set PERL5LIB ${with perlPackages; makeFullPerlPath [ ListAllUtils ]}
   '';
-
   dontStrip = true;
 }
