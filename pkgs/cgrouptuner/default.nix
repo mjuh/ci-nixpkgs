24bf48709fac4e882cf66105f41c2cfc20ade0d4
Oleg Pykhalov <go.wigust@gmail.com>
2021-01-28 17:02:16 +0300

    pkgs: cgrouptuner: Fix shebang on wrapped program.

--- pkgs/cgrouptuner/default.nix
+++ pkgs/cgrouptuner/default.nix
@@ -3,10 +3,11 @@
 stdenv.mkDerivation {
   name = "cgrouptuner";
   src = ./src;
-  nativeBuildInputs = [ makeWrapper ];
+  nativeBuildInputs = [ makeWrapper perlPackages.perl ];
   installPhase = ''
     mkdir -p $out/bin
     install -m755 cgrouptuner.pl $out/bin/cgrouptuner
+    patchShebangs $out/bin/cgrouptuner
     wrapProgram $out/bin/cgrouptuner --set PERL5LIB ${with perlPackages; makeFullPerlPath [ ListAllUtils ]}
   '';
   dontStrip = true;
