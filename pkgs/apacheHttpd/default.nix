ace1a8029b91a66a727c4f2ffeabb7329edfb7d6
Oleg Pykhalov <go.wigust@gmail.com>
2022-04-25 13:53:09 +0300

    pkgs: apacheHttpd: Format with nixpkgs-fmt.

--- pkgs/apacheHttpd/default.nix
+++ pkgs/apacheHttpd/default.nix
@@ -1,50 +1,54 @@
 { stdenv, lib, fetchurl, apr, aprutil, perl, zlib, nss_ldap, nss_pam_ldapd, openldap, pcre, openssl, sslSupport ? false }:
 
 stdenv.mkDerivation rec {
-      version = "2.4.46";
-      name = "apache-httpd-${version}";
-      src = fetchurl {
-          url = "mirror://apache/httpd/httpd-${version}.tar.bz2";
-          sha256 = "1sj1rwgbcjgkzac3ybjy7j68c9b3dv3ap71m48mrjhf6w7vds3kl";
-      };
-      outputs = [ "out" "dev" ];
-      setOutputFlags = false; # it would move $out/modules, etc.
-      buildInputs = [ 
-        perl zlib nss_ldap nss_pam_ldapd openldap 
-      ] ++ lib.optional sslSupport openssl;
-      prePatch = ''
-          sed -i config.layout -e "s|installbuilddir:.*|installbuilddir: $dev/share/build|"
-      '';
+  version = "2.4.46";
+  name = "apache-httpd-${version}";
+  src = fetchurl {
+    url = "mirror://apache/httpd/httpd-${version}.tar.bz2";
+    sha256 = "1sj1rwgbcjgkzac3ybjy7j68c9b3dv3ap71m48mrjhf6w7vds3kl";
+  };
+  outputs = [ "out" "dev" ];
+  setOutputFlags = false; # it would move $out/modules, etc.
+  buildInputs = [
+    perl
+    zlib
+    nss_ldap
+    nss_pam_ldapd
+    openldap
+  ] ++ lib.optional sslSupport openssl;
+  prePatch = ''
+    sed -i config.layout -e "s|installbuilddir:.*|installbuilddir: $dev/share/build|"
+  '';
 
-      preConfigure = ''
-          configureFlags="$configureFlags --includedir=$dev/include"
-      '';
+  preConfigure = ''
+    configureFlags="$configureFlags --includedir=$dev/include"
+  '';
 
-      configureFlags = [
-          "--with-apr=${apr.dev}"
-          "--with-apr-util=${aprutil.dev}"
-          "--with-z=${zlib.dev}"
-          "--with-pcre=${pcre.dev}"
-          "--disable-maintainer-mode"
-          "--disable-debugger-mode"
-          "--enable-mods-shared=all"
-          "--enable-mpms-shared=all"
-          "--enable-cern-meta"
-          "--enable-imagemap"
-          "--enable-cgi"
-          "--disable-ldap"
-          "--with-mpm=prefork"
-          (lib.enableFeature sslSupport "ssl")
-      ];
+  configureFlags = [
+    "--with-apr=${apr.dev}"
+    "--with-apr-util=${aprutil.dev}"
+    "--with-z=${zlib.dev}"
+    "--with-pcre=${pcre.dev}"
+    "--disable-maintainer-mode"
+    "--disable-debugger-mode"
+    "--enable-mods-shared=all"
+    "--enable-mpms-shared=all"
+    "--enable-cern-meta"
+    "--enable-imagemap"
+    "--enable-cgi"
+    "--disable-ldap"
+    "--with-mpm=prefork"
+    (lib.enableFeature sslSupport "ssl")
+  ];
 
-      enableParallelBuilding = true;
-      stripDebugList = "lib modules bin";
-      postInstall = ''
-          mkdir -p $dev/bin
-          mv $out/bin/apxs $dev/bin/apxs
-      '';
+  enableParallelBuilding = true;
+  stripDebugList = "lib modules bin";
+  postInstall = ''
+    mkdir -p $dev/bin
+    mv $out/bin/apxs $dev/bin/apxs
+  '';
 
-      passthru = {
-          inherit apr aprutil ;
-      };
+  passthru = {
+    inherit apr aprutil;
+  };
 }
